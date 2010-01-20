f34830010844ff717a3d8d93c6f62a84bb752f91
angelo.bertolli <angelo.bertolli@2b30a496-dcb1-11de-b4bf-3fb2a63344f9>
2010-01-20 06:53:40 +0000

    Tag dragonskin version 1.3 with children

---
+++ tags/1.3/dragonskin/footer.php
@@ -0,0 +1,30 @@
+<div class="clear_content"></div>
+
+</div>
+
+<div id="footer">
+
+<p class="credit">
+
+<?php /*
+Under the license for this theme, you are allowed to completely modify any of this code.  However,
+please leave the link to let people know where to get this theme, or provide a link in your blogroll.
+*/ ?>
+
+<br />
+
+<a href="http://wordpress.org/"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/wordpress-80x15.png" alt="Powered by WordPress" border="0" /></a>
+<a href="http://bitfreedom.com/dragonskin/"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/dragonskin-80x15.png" alt="Download the Dragonskin Theme" border="0" /></a>
+<!--
+<a href="http://heroesonly.com/"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/heroesonly-80x15.png" alt="Theme created by Heroes Only" border="0" /></a>
+-->
+</p>
+
+</div>
+
+<?php wp_footer(); ?>
+
+<?php if (is_readable(TEMPLATEPATH."/clickheat.php")) include(TEMPLATEPATH."/clickheat.php"); ?>
+
+</body>
+</html>
