b3651809675f67251e2db22ba31c312f1d4052e6
angelo.bertolli <angelo.bertolli@2b30a496-dcb1-11de-b4bf-3fb2a63344f9>
2010-07-11 01:03:04 +0000

    Added support for skincolor.css (if it exists).

--- header.php
+++ header.php
@@ -8,7 +8,10 @@
 	<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico">
 
 	<style type="text/css" media="screen">
-		@import url( <?php bloginfo('stylesheet_url'); ?> );
+	@import url( <?php bloginfo('stylesheet_url'); ?> );
+	<?php if (is_readable(get_stylesheet_directory()."/skincolor.css")) { ?>
+	@import url( <?php echo get_bloginfo('stylesheet_directory')."/skincolor.css"; ?> );
+	<?php } ?>
 	</style>
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
