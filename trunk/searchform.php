20f60cb657b909d3c99c3bd2aaa036ae29e09f54
angelo.bertolli <angelo.bertolli@2b30a496-dcb1-11de-b4bf-3fb2a63344f9>
2010-11-21 02:26:45 +0000

    Changed deprecated code

--- trunk/searchform.php
+++ trunk/searchform.php
@@ -2,7 +2,7 @@
 <br />
 <div>
 <form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
-<input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" alt="search" size="60" />
+<input type="text" value="<?php esc_html_e($s); ?>" name="s" id="s" alt="search" size="60" />
 <input type="submit" id="searchsubmit" value="Search" />
 </form>
 
