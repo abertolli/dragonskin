b1e01bfe505c72965b0072e2742522fa3fc7e346
angelo.bertolli <angelo.bertolli@2b30a496-dcb1-11de-b4bf-3fb2a63344f9>
2011-01-26 07:40:19 +0000

    Admin-side custom header image support

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 $content_width = 480;
 add_theme_support( 'automatic-feed-links' );
 
+
 if ( function_exists('register_sidebar') )
     register_sidebar();	
     
@@ -194,18 +195,61 @@ if ($value['type'] == "text") { ?>
 <?php
 }
 
-function mytheme_wp_head() { 
+function get_css_filename() {
 	global $options, $all_styles; 
 	foreach ($options as $value) {
         if (get_option( $value['id'] ) === FALSE) { $$value['id'] = $value['std']; } else { $$value['id'] = get_option( $value['id'] ); }
 		foreach ($all_styles as $style) {
-			if ($ds_style_chooser == $style['Name'] or $ds_style_chooser == $style['CSS']) $theme = "skins/".$style['CSS'];
+			if ($ds_style_chooser == $style['Name'] or $ds_style_chooser == $style['CSS']) $filename = $style['CSS'];
 		}
-       
-?>
-<link href="<?php echo get_stylesheet_directory_uri()."/$theme"; ?>" rel="stylesheet" type="text/css" />
-<?php  }
+	}
+	return $filename;
+}
+
+function mytheme_wp_head() { ?>
+<link href="<?php echo get_stylesheet_directory_uri()."/skins/".get_css_filename(); ?>" rel="stylesheet" type="text/css" />
+<?php
 }
 
 add_action('wp_head', 'mytheme_wp_head');
-add_action('admin_menu', 'mytheme_add_admin'); ?>
+add_action('admin_menu', 'mytheme_add_admin');
+
+// Header Image
+
+define('HEADER_TEXTCOLOR', 'ccc');
+define('HEADER_IMAGE', '');
+define('HEADER_IMAGE_WIDTH', 750);
+define('HEADER_IMAGE_HEIGHT', 200);
+
+// gets included in the site header
+function header_style() {
+    ?><style type="text/css">
+        #header {
+            background: url(<?php header_image(); ?>);
+        }
+    </style><?php
+}
+
+// gets included in the admin header
+function admin_header_style() {
+?>
+<style type="text/css">
+	<?php if (get_header_image() == "") { ?>
+	#headimg, #headimg h1 a {
+		text-align: center;
+		font-family: Garamond, 'Palatino Linotype', 'Book Antiqua', Georgia, 'MS Serif', Serif;
+		text-decoration: none;
+		background: #444;
+	}
+	<?php } else { ?>
+	#headimg h1 a, #headimg #desc {
+		display: none;
+	}
+	<?php } ?>
+    </style>
+<?php
+}
+
+add_custom_image_header('header_style', 'admin_header_style');
+
+?>
